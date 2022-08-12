95f9d9c235935dd5bc2556415d140c894786eefa
nkere30 <ninokereselidze30@gmail.com>
2022-08-12 17:43:14 +0400

    Completed

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -1,7 +1,23 @@
 package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
-    public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
+    public static void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
+        int counter = 1;
+        if(numberToGoUntil < toBreakWith) {
+            System.out.println("iterating till the end");
+            do {
+                System.out.println(counter);
+                counter++;
+            } while(counter <= numberToGoUntil);
+        } else {
+            do {
+                System.out.println(counter);
+                counter++;
+            } while(counter <= toBreakWith);
+        }
+    }
 
+    public static void main(String[] args) {
+        printNumbersUntilWithBreakOn(6, 5);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -1,7 +1,34 @@
 package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
-    public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
+    public static void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
+        int counter = 0;
+        int skippedSum = 0;
+        int countedSum = 0;
+        do {
+            if(counter != numberToSkip) {
+                countedSum +=counter;
+            } else {
+                skippedSum = countedSum + numberToSkip;
+                countedSum = 0;
+            }
+            counter++;
+        } while(counter <= lastInRow);
 
+        if(lastInRow < 0) {
+            System.out.println("last number in row is negative");
+        } else if(numberToSkip > lastInRow) {
+            System.out.println("number to skip is bugger then the last");
+        } else {
+            System.out.println("skipped sum is number " + skippedSum);
+            System.out.println("counted sum is number " + countedSum);
+        }
+    }
+
+    public static void main(String[] args) {
+        printSumOfTwoRanges(5, 10);
+        printSumOfTwoRanges(15, 10);
+        printSumOfTwoRanges(10, 10);
+        printSumOfTwoRanges(-10, -1);
     }
 }
